61c2225b160b3af26de452da0247bd7f14573110
mariarezende1112 <131001998+mariarezende1112@users.noreply.github.com>
2024-06-03 09:04:43 -0300

    Add files via upload

---
+++ objetos/Aula01/objetos.js
@@ -0,0 +1,8 @@
+const informacoesPessoa = ["nome","duda","idade",17,"CPF","11245978252"];
+
+const objetoPessoa = {
+    nome:"duda",
+    idade: 17,
+    CPF: "11245978252",
+    email: "mariaeduardarp7@icloud.com",
+}
