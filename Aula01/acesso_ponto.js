3a79b671d5d36eca84c94a81aa1d8d2cff06efb7
mariarezende1112 <131001998+mariarezende1112@users.noreply.github.com>
2024-06-10 09:07:00 -0300

    Add files via upload

---
+++ Aula01/acesso_ponto.js
@@ -0,0 +1,8 @@
+
+const objetoPessoa = {
+    nome:"duda",
+    idade: 17,
+    CPF: "11245978252",
+    email: "mariaeduardarp7@icloud.com",
+}
+console.log(objetoPessoa)
---
+++ Aula02/tipo.js
@@ -0,0 +1,7 @@
+const cliente = {
+    nome: "maria",
+    idade: 17,
+    email: "mariarp@email.com",
+    telefone: ["(43)984557861","(43)984796158"],
+};
+console.log(cliente);
